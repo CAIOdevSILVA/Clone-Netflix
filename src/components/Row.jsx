ab0483d6c08dafcee340da58d57a9f48d9c8b73b
Caio Silva <caiossantosp5560@gmail.com>
2023-04-03 08:44:34 -0300

    Finalizando o Projeto

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,11 +2,34 @@ import React, { useState, useEffect } from 'react'
 import { getMovies } from "../services/api"
 
 import "./Row.css"
+import ReactPlayer from 'react-player'
+import movieTrailer from 'movie-trailer'
 
 const imageHost = "https://image.tmdb.org/t/p/original/"
 
 const Row = ({ title, path, isLarge }) => {
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState('')
+
+  const handleOnClick = (movie) => {
+    if(trailerUrl){
+      setTrailerUrl("")
+    }else{
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+      .then((url) => {
+        setTrailerUrl(url)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+      console.log({
+        name: movies?.name || "",
+        title: movies?.title || "",
+        original: movies?.original_name || ""
+      })
+    }
+  }
 
   const fetchMovies  = async (_path) => {
     try {
@@ -30,11 +53,13 @@ const Row = ({ title, path, isLarge }) => {
         {movies?.map((movie) => (
           <img 
             className={ isLarge ? "movie-card-large" : "movie-card" }
+            onClick={() => handleOnClick(movie)}
             key={movie.id}
-            src={`${imageHost}${isLarge ? movie.backdrop_path : movie.poster_path}`} 
+            src={`${imageHost}${isLarge ? movie?.backdrop_path : movie?.poster_path}`} 
             alt={movie.name}/>
         ))}
       </div>
+      {trailerUrl && <ReactPlayer url={trailerUrl}/>}
     </div>
   )
 }
